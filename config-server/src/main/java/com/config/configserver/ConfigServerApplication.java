89f719b462621c81bc436e8181a327754cd0b078
jerry <jm90_yang@163.com>
2018-09-30 13:55:01 +0800

    Initial commit

---
+++ config-server/src/main/java/com/config/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.config.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * zuul-dev.properties
+ * 访问URL:http://localhost:8888/zuul/dev(URL路径与文件名对应)
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
